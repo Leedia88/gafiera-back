fcb996edf297d776a551e5ae4bc1507bd0090629
Leedia88 <lydia.lachenaud@gmail.com>
2023-12-19 10:10:02 +0100

    adding Id for Role

--- src/main/java/com/aicap/springbootwebapi/entity/Role.java
+++ src/main/java/com/aicap/springbootwebapi/entity/Role.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @AllArgsConstructor
 public class Role {
+
+    @Id
+    private Long id;
 
     private String name;
 }
